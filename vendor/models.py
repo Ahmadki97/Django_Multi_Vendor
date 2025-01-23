31d353b0bf3ad35f5979de0e0777727408fe453f
Ahmadki97 <ahmad.s.kirata97@gmail.com>
2025-01-23 15:52:28 +0300

    Vendor Approval By Admin

--- vendor/models.py
+++ vendor/models.py
@@ -1,11 +1,12 @@
 from django.db import models
 from accounts.models import User, UserProfile
+from accounts.utils import sendNotificationMail
 
 # Create your models here.
 
 class Vendor(models.Model):
     user = models.OneToOneField(User, related_name='user', on_delete=models.CASCADE)
-    user_profle = models.OneToOneField(UserProfile, related_name='user_profle', on_delete=models.CASCADE)
+    user_profile = models.OneToOneField(UserProfile, related_name='user_profle', on_delete=models.CASCADE)
     vendor_name = models.CharField(max_length=50)
     vendor_license = models.ImageField(upload_to='vendor/license')
     is_approved = models.BooleanField(default=False)
@@ -16,3 +17,24 @@ class Vendor(models.Model):
 
     def __str__(self):
         return self.vendor_name
+    
+
+    def save(self, *args, **kwargs):
+        if self.pk is not None:
+            print(self.pk)
+            orig = Vendor.objects.get(pk=self.pk)
+            print(f"Original is_approved: {orig.is_approved}, Current is_approved: {self.is_approved}")
+            if orig.is_approved != self.is_approved:
+                print(f"Second if executed")
+                mail_template = 'accounts/emails/admin_approval_email.html'
+                context = {
+                    'user': self.user,
+                    'is_approved': self.is_approved
+                }
+                if self.is_approved == True:
+                    mail_subject = "Congratulations, Your resturan has been approved."
+                    sendNotificationMail(mail_subject=mail_subject, mail_template=mail_template, context=context)
+                else:
+                    mail_subject = 'We Are sorry! you are not eligible for publishing your food menu on our marketplace'
+                    sendNotificationMail(mail_subject=mail_subject, mail_template=mail_template, context=context)
+        return super(Vendor, self).save(*args, **kwargs) 
--- vendor/views.py
+++ vendor/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .models import Vendor
 # Create your views here.
+
+
+def profile(request):
+    return render(request, 'vendor/vprofile.html')
